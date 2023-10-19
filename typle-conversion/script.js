3e5b340ca7c3ac09e1c0c99853821fd47b092b1d
Changho Youn <younch8342@yu.ac.kr>
2023-10-19 11:00:48 +0900

    js 타입변환 알아보기

---
+++ typle-conversion/script.js
@@ -0,0 +1,14 @@
+// type conversion
+ let now = new Date();
+ console.log(now);
+ console.log(typeof now);
+
+ now = String(now);
+ console.log(now);
+ console.log(typeof now);
+
+ /* 
+ parseInt
+ parseFloat 
+ parseLong ...and so on
+ */
