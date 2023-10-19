31e90a9d344a54de5a2dc48cb5018ea0e8121f1f
Changho Youn <younch8342@yu.ac.kr>
2023-10-19 10:18:02 +0900

    var 사용금지, let 사용하기

---
+++ var,let,const/script.js
@@ -0,0 +1,13 @@
+/* // var a = 1;
+// console.log(a);
+
+// a = 'changho';
+// console.log(a);
+
+// a = false;
+// console.log(a);  */
+
+let greeting = 'hi';
+// let greeting = 'my name';
+/* let의 경우, 
+변수명 재선언 불가능 */
